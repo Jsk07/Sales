28e5cd1b7d9e87900c5bfe0ea35d318c355ab577
Jsk07 <67358878+Jsk07@users.noreply.github.com>
2020-06-25 01:15:00 -0500

    Project create

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")// Conexión DB
+        {
+
+        }
+    }
+}
